470568f8d677f73db47c709099f1743b5608b463
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-22 10:23:30 +0200

    Add index action to show project overview.

---
+++ PortfolioWeb/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.EntityFrameworkCore;
+using PortfolioWeb.Data;
+using PortfolioWeb.Models;
+
+namespace PortfolioWeb.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ApplicationDbContext _portfolioDbContext;
+
+        public ProjectController(ApplicationDbContext applicationDbContext)
+        {
+            _portfolioDbContext = applicationDbContext;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var projects = await _portfolioDbContext.Projects
+                .Include(x => x.Status)
+                .Where(x => userId == x.PortfolioUserId)
+                .ToListAsync();
+
+            return View(projects.Select(item => new ProjectIndexViewModel 
+            { 
+                Id = item.Id,
+                Name = item.Name,
+                Status = item.Status.Description,
+                StartDate = item.StartDate,
+                EndDate = item.EndDate
+            }));
+        }
+    }
+}
---
+++ PortfolioWeb/Models/ProjectIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PortfolioWeb.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWeb.Models
+{
+    public class ProjectIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
