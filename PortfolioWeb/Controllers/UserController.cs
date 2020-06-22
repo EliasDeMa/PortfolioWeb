1ce0c7eda54528c463f2dcb62d1565b7f70d7c04
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-22 14:55:51 +0200

    Allow user to see their info.

---
+++ PortfolioWeb/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioWeb.Data;
+using PortfolioWeb.Models;
+
+namespace PortfolioWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _portfolioDbContext;
+
+        public UserController(ApplicationDbContext portfolioDbContext)
+        {
+            _portfolioDbContext = portfolioDbContext;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var userDb = await _portfolioDbContext.Users.FirstOrDefaultAsync(x => x.Id == userId);
+
+            var user = new UserDetailViewModel
+            {
+                Name = userDb.UserName,
+                Description = userDb.Description,
+            };
+
+            return View(user);
+        }
+    }
+}
