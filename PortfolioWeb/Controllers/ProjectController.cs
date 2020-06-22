1dbaeeb8b7cbfaeccf81059b921f50d2e85fcb77
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-22 11:24:46 +0200

    Add ability to create new project.

--- PortfolioWeb/Controllers/ProjectController.cs
+++ PortfolioWeb/Controllers/ProjectController.cs
@@ -5,20 +5,25 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.EntityFrameworkCore;
 using PortfolioWeb.Data;
+using PortfolioWeb.Domain;
 using PortfolioWeb.Models;
+using PortfolioWeb.Services;
 
 namespace PortfolioWeb.Controllers
 {
     public class ProjectController : Controller
     {
-        ApplicationDbContext _portfolioDbContext;
+        private readonly ApplicationDbContext _portfolioDbContext;
+        private readonly IPhotoService _photoService;
 
-        public ProjectController(ApplicationDbContext applicationDbContext)
+        public ProjectController(ApplicationDbContext applicationDbContext, IPhotoService photoService)
         {
             _portfolioDbContext = applicationDbContext;
+            _photoService = photoService;
         }
 
         [Authorize]
@@ -39,6 +44,59 @@ namespace PortfolioWeb.Controllers
                 StartDate = item.StartDate,
                 EndDate = item.EndDate
             }));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Create()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var statuses = await _portfolioDbContext.Statuses.ToListAsync();
+            var tags = await _portfolioDbContext.Tags.ToListAsync();
+
+            var vm = new ProjectCreateViewModel { 
+                Statuses = statuses.Select(item => new SelectListItem
+                {
+                    Value = item.Id.ToString(),
+                    Text = item.Description
+                }),
+                Tags = tags.Select(item => new SelectListItem
+                {
+                    Value = item.Id.ToString(),
+                    Text = item.Name
+                })
+            };
+
+            return View(vm);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProjectCreateViewModel vm)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var project = new Project
+            {
+                Name = vm.Name,
+                Description = vm.Description,
+                StartDate = vm.StartDate,
+                EndDate = vm.EndDate,
+                StatusId = vm.SelectedStatus,
+                ProjectTags = vm.SelectedTags.Select(id => new ProjectTag { TagId = id }).ToList(),
+                PortfolioUserId = userId
+            };
+
+            if (vm.File != null)
+            {
+                project.PhotoUrl = _photoService.AddPhoto(vm.File);
+            }
+
+            await _portfolioDbContext.AddAsync(project);
+            await _portfolioDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PortfolioWeb/Models/ProjectCreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PortfolioWeb.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWeb.Models
+{
+    public class ProjectCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public int SelectedStatus { get; set; }
+        public int[] SelectedTags { get; set; }
+
+        public IEnumerable<SelectListItem> Statuses { get; set; }
+        public IEnumerable<SelectListItem> Tags { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
