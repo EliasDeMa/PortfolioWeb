1ab5719d4db4cac6e4ed31faad8f9da4a230cf4f
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-22 11:23:18 +0200

    Add service to save photos.

---
+++ PortfolioWeb/Services/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PortfolioWeb.Services
+{
+    public interface IPhotoService
+    {
+        string AddPhoto(IFormFile file);
+        void DeletePhoto(string fileName);
+    }
+}
---
+++ PortfolioWeb/Services/PhotoService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWeb.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public PhotoService(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public string AddPhoto(IFormFile photo)
+        {
+            string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(photo.FileName);
+            var path = Path.Combine(_hostEnvironment.WebRootPath, "expense-pics", uniqueFileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            photo.CopyTo(stream);
+
+            return "/expense-pics/" + uniqueFileName;
+        }
+
+        public void DeletePhoto(string fileName)
+        {
+            var prevPath = Path.Combine(_hostEnvironment.WebRootPath, "expense-pics", fileName.Replace("/expense-pics/", ""));
+            System.IO.File.Delete(prevPath);
+        }
+    }
+}
